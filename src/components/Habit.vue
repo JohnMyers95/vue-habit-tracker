d98b4bd1d20bf9cd66ca03a29d61792d805f6cb7
John M <johnpmyers1995@gmail.com>
2021-02-20 22:21:23 +0000

    Added: Pretty colour transition on completion

--- src/components/Habit.vue
+++ src/components/Habit.vue
@@ -1,52 +1,65 @@
 <template>
-    <div class="habit" @click="addProgress">
-        <div class="title">
-            {{habit.title}}
-        </div>
-        <div class="progress">
-           <h1>{{this.progress}} / {{habit.goal}} </h1>
-        </div>
+  <div :class="cssClass" @click="addProgress">
+    <div class="title">
+      {{ habit.title }}
     </div>
+    <div class="progress">{{ this.progress }} / {{ habit.goal }}</div>
+  </div>
 </template>
 <script>
 export default {
-    data: () => {
-        return {
-            progress: 0,
-        }
+  data: () => {
+    return {
+      progress: 0,
+      cssClass: "habit",
+    };
+  },
+  props: {
+    habit: {
+      type: Object,
+      required: true,
     },
-    props: {
-        habit: {
-            type: Object,
-            required: true
-        }
+  },
+  methods: {
+    addProgress() {
+      if (this.progress < this.habit.goal) {
+        this.progress++;
+      }
+      if (this.progress == this.habit.goal) {
+        this.cssClass = "habit completed";
+      }
     },
-    methods: {
-        addProgress() {
-            if(this.progress < this.habit.goal) {
-                this.progress++;
-            }
-        }
-    },
-    created: () => {
-
-    }
-}
+  },
+  created: () => {},
+};
 </script>
 
 <style scoped>
 .habit {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 9fr;
-    border-radius: 30px;
-    background-color:palegoldenrod;
-    aspect-ratio: 1/1;
-    border: 1px solid black;
-    box-shadow: 4px 4px 2px 1px #8a8a8a;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 2fr;
+  border-radius: 30px;
+  background-color: palegoldenrod;
+  aspect-ratio: 1/1;
+  border: 0.5px solid black;
+  box-shadow: 4px 4px 2px 1px #8a8a8a;
+  padding: 10px;
+  align-items: center;
+  transition: 0.5s;
 }
-.title{
-    display:flex;
-    justify-content: center;
+.title {
+  display: flex;
+  justify-content: center;
+  margin: 5px auto;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+.progress {
+  font-size: 2em;
+  font-weight: bold;
+}
+.completed {
+  background-color: greenyellow;
 }
 </style>
